b80bb1fdd045d0614d49808d49fad9969243e115
daslu <daniel.slutsky@gmail.com>
2024-01-11 09:50:26 +0200

    version "1-alpha6"

--- build.clj
+++ build.clj
@@ -15,6 +15,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/kindly-advice)
+
 (def version "1-alpha6")
 (def snapshot (str version "-SNAPSHOT"))
 (def class-dir "target/classes")
